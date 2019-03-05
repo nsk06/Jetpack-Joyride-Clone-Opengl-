a4822cc16f09ac633bbfd5f972173a118e0c11dc
Nsk06(Nonidh Singh) <nonidh.singh@gmail.com>
2019-03-05 22:53:02 +0530

    Add files via upload

---
+++ magnet.h
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#ifndef MAGNET_H
+#define MAGNET_H
+
+
+class Magnet {
+public:
+    Magnet() {}
+    Magnet(float x, float y);
+    glm::vec3 position;
+    float rotation;
+    void draw(glm::mat4 VP);
+    void set_position(float x, float y);
+    void tick();
+   int points;
+   double speed;
+private:
+    VAO *centre;
+    VAO *front;
+};
+
+#endif // BALL_H
