c58355b2fd4d37dd5aa6ccbf181a7bbd2f0c960e
Nsk06(Nonidh Singh) <nonidh.singh@gmail.com>
2019-03-05 22:48:54 +0530

    Add files via upload

---
+++ boomerang.h
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#ifndef BOOMERANG_H
+#define BOOMERANG_H
+
+
+class Boomerang {
+public:
+    Boomerang() {}
+    Boomerang(float x, float y);
+    glm::vec3 position;
+    float rotation;
+    void draw(glm::mat4 VP);
+    void set_position(float x, float y);
+    void tick();
+   int pos;
+   double speed;
+   double shift;
+   double spawnpos;
+private:
+    VAO *shape;
+};
+
+#endif // BALL_H
