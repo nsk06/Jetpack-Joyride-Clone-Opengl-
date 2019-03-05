a65d56a7f6b099b35c79b2a4e2a61f5c94014747
Nsk06(Nonidh Singh) <nonidh.singh@gmail.com>
2019-03-05 22:56:38 +0530

    Add files via upload

---
+++ ring.h
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#ifndef RING_H
+#define RING_H
+
+
+class Ring {
+public:
+    Ring() {}
+    Ring(float x, float y);
+    glm::vec3 position;
+    float rotation;
+    void draw(glm::mat4 VP);
+    void set_position(float x, float y);
+    void tick();
+   int points;
+   double speed;
+private:
+    VAO *inner;
+    VAO * outer;
+};
+
+#endif // BALL_H
